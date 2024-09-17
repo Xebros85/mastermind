3cc030de86854e8b4cf23215e95b33166ea37f26
Xebros85 <165695942+Xebros85@users.noreply.github.com>
2024-09-17 13:02:43 -0500

    Added notes to how the board should behave

--- main.rb
+++ main.rb
@@ -3,6 +3,28 @@ require_relative 'lib/computer.rb'
 require_relative 'lib/gameboard.rb'
 require_relative 'lib/player.rb'
 
+# What do I want the board to look like?
+# [o, o ,o, o] ?? How to display?
+
+# update board function
+
+# Instead of o use the first letter of the color?
+
+# [o, o ,o, o]
+# [o, o ,o, o]
+# [o, o ,o, o]
+# [o, o ,o, o]
+# [o, o ,o, o]
+# [o, o ,o, o]
+# [o, o ,o, o]
+# [o, o ,o, o]
+# [o, o ,o, o]
+# [o, o ,o, o]
+
+# White is incorrect if not in array
+# Orange is in array but not in the right spot
+# Green when in array AND in correct position
+
 
 player = Player.new
 computer = Computer.new
